3d4aa2fe9408c6b7d9c5b3fb8a5eccb7998ab65e
tom_glb <goodMorning_glb.atgigu.com>
2019-04-14 22:38:09 +0800

    Add StackAndQueue method for Stack and Queue

---
+++ StackAndQueueConvert/StackAndQueueConvert/TwoQueueStack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueConvert
+{
+    class TwoQueueStack
+    {
+        private Queue<int> data;
+        private Queue<int> help;
+
+        public TwoQueueStack()
+        {
+            data = new Queue<int>();
+            help = new Queue<int>();
+        }
+
+        public void push(int pushInt)
+        {
+            data.Enqueue(pushInt);
+        }
+
+        public int peek()
+        {
+            if (data.Count == 0)
+            {
+                throw new Exception("Stack is empty!");
+            }
+            while (data.Count != 1)
+            {
+                help.Enqueue(data.Dequeue());
+            }
+            int res = data.Dequeue();
+            help.Enqueue(res);
+            swap();
+            return res;
+        }
+
+        public int pop()
+        {
+            if (data.Count == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+            while (data.Count > 1)
+            {
+                help.Enqueue(data.Dequeue());
+            }
+            int res = data.Dequeue();
+            swap();
+            return res;
+        }
+
+        private void swap()
+        {
+            Queue<int> tmp = help;
+            help = data;
+            data = tmp;
+        }
+    }
+}
---
+++ StackAndQueueConvert/StackAndQueueConvert/TwoStackQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueConvert
+{
+    class TwoStackQueue
+    {
+        private Stack<int> stackPush;
+        private Stack<int> stackPop;
+
+        public TwoStackQueue()
+        {
+            stackPush = new Stack<int>();
+            stackPop = new Stack<int>();
+        }
+
+        public void push(int pushInt)
+        {
+            stackPush.Push(pushInt);
+        }
+
+        public int poll()
+        {
+            if(stackPop.Count == 0 && stackPush.Count == 0)
+            {
+                throw new Exception("Queue is empty");
+            }
+            else if(stackPop.Count == 0)
+            {
+                while(!(stackPush.Count == 0))
+                {
+                    stackPop.Push(stackPush.Pop());
+                }
+            }
+            return stackPop.Peek();
+        }
+
+        public int peek()
+        {
+            if(stackPop.Count == 0 && stackPush.Count == 0)
+            {
+                throw new Exception("Queue is empty");
+            }
+            else if(stackPop.Count == 0)
+            {
+                while(!(stackPush.Count == 0))
+                {
+                    stackPop.Push(stackPush.Pop());
+                }
+            }
+            return stackPop.Peek();
+        }
+    }
+}
